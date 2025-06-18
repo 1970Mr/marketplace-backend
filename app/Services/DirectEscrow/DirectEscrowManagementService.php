a147e287d4bbe9db69c1623b6f07baf36564f1f0
1970Mr <mrepoch1970@gmail.com>
2025-06-19 01:15:07 +0330

    Add correct filters for direct escrow

--- app/Services/DirectEscrow/DirectEscrowManagementService.php
+++ app/Services/DirectEscrow/DirectEscrowManagementService.php
@@ -20,6 +20,10 @@ class DirectEscrowManagementService
         return $user->escrows()
             ->where('type', EscrowType::DIRECT->value)
             ->with(['offer.product', 'buyer', 'seller', 'directEscrow'])
+            ->filterByProductTitle($request->get('search'))
+            ->filterByRelationColumn('status', $request->get('status'))
+            ->filterByRelationColumn('phase', $request->get('phase'))
+            ->filterByRelationColumn('stage', $request->get('stage'))
             ->filterByDateRange($request->get('from_date'), $request->get('to_date'))
             ->hasUnreadMessages((bool)$request->get('need_response', false))
             ->latest()
@@ -49,6 +53,8 @@ class DirectEscrowManagementService
     {
         return Escrow::with(['offer.product', 'buyer', 'seller', 'directEscrow'])
             ->where('type', EscrowType::DIRECT->value)
+            ->filterByProductTitle($request->get('search'))
+            ->filterByDateRange($request->get('from_date'), $request->get('to_date'))
             ->latest()
             ->paginate($request->get('per_page', 10));
     }
@@ -58,6 +64,8 @@ class DirectEscrowManagementService
         return Escrow::with(['offer.product', 'buyer', 'seller', 'directEscrow'])
             ->where('type', EscrowType::DIRECT->value)
             ->whereNull('admin_id')
+            ->filterByProductTitle($request->get('search'))
+            ->filterByDateRange($request->get('from_date'), $request->get('to_date'))
             ->latest()
             ->paginate($request->get('per_page', 10));
     }
